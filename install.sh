eb4294f7f4aecbad0221b0351c1c43d280cc9bb6
daisuke23 <daisuke0x17@gmail.com>
2024-01-19 09:37:21 +0900

    refactor: omit s3 api

--- install.sh
+++ install.sh
@@ -12,10 +12,8 @@ LAMBDANAME="$LAMBDANAME-$RAND"
 ROLENAME="$LAMBDANAME-role"
 # ECR Repository - lambda container image stored
 REPOSITORYNAME=$(echo "$LAMBDANAME-repo" | tr 'A-Z' 'a-z')
-# S3 Bucket where the created images will be stored
-BUCKETNAME=$(echo "$LAMBDANAME-bucket" | tr 'A-Z' 'a-z')
 
-echo "(1/6) Create AWS ECR Repository"
+echo "(1/5) Create AWS ECR Repository"
 RESULT=$(aws ecr create-repository --repository-name ${REPOSITORYNAME})
 if [ "`echo $RESULT | grep 'repositoryArn'`" ]; then
   echo "Success!!: Create Repository"
@@ -24,41 +22,36 @@ else
   exit
 fi
 
-echo "(2/6) Create Role"
+echo "(2/5) Create Role"
 RESULT=$(aws iam create-role --role-name ${ROLENAME} --assume-role-policy-document '{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}')
 aws iam attach-role-policy --role-name ${ROLENAME} --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
 aws iam attach-role-policy --role-name ${ROLENAME} --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
 
-echo "(3/6) Build Container"
+echo "(3/5) Build Container"
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNTID}.dkr.ecr.${REGION}.amazonaws.com
-# print log: --progress=plain
-docker build --platform=linux/amd64 --no-cache -t ${REPOSITORYNAME} .
-# docker build --no-cache -t ${REPOSITORYNAME} -f ./Dockerfile.waifu .
+# --progress=plain
+# --platform=linux/amd64
+docker build -t ${REPOSITORYNAME} .
 docker tag ${REPOSITORYNAME}:latest ${ACCOUNTID}.dkr.ecr.${REGION}.amazonaws.com/${REPOSITORYNAME}:latest
 
-echo "(4/6) Push Container"
+echo "(4/5) Push Container"
 docker push ${ACCOUNTID}.dkr.ecr.${REGION}.amazonaws.com/${REPOSITORYNAME}:latest
 
-echo "(5/6) Create S3 Bucket"
-aws s3api create-bucket --bucket ${BUCKETNAME} --region ${REGION} --create-bucket-configuration LocationConstraint=${REGION}
-
-echo "(6/6) Create Lambda"
+echo "(5/5) Create Lambda"
 ROLE_ARN=arn:aws:iam::${ACCOUNTID}:role/${ROLENAME}
 DIGEST=$(aws ecr list-images --repository-name ${REPOSITORYNAME} --out text --query 'imageIds[?imageTag==`latest`].imageDigest')
 aws lambda create-function --function-name ${LAMBDANAME} --out text \
     --package-type Image --code ImageUri=${ACCOUNTID}.dkr.ecr.${REGION}.amazonaws.com/${REPOSITORYNAME}@${DIGEST} \
-    --memory-size 10240 --timeout 600 --environment Variables={"BUCKET"="${BUCKETNAME}"} \
+    --memory-size 10240 --timeout 600 Size=5120 \
     --role ${ROLE_ARN}
 
 # uninstaller
 cat ./uninstall.txt | sed -e "s/BEFORELAMBDANAME/${LAMBDANAME}/" \
     -e "s/BEFOREROLENAME/${ROLENAME}/" \
-    -e "s/BEFOREREPOSITORYNAME/${REPOSITORYNAME}/" \
-    -e "s/BEFOREBUCKETNAME/${BUCKETNAME}/" > ./"uninstall-$RAND.sh"
+    -e "s/BEFOREREPOSITORYNAME/${REPOSITORYNAME}/" > ./"uninstall-$RAND.sh"
 
 echo "******* Complete!! *******"
 echo "The following resources were created."
 echo "- Lmabda function: $LAMBDANAME"
 echo "- Role: $ROLENAME"
 echo "- ECR Repository: $REPOSITORYNAME"
-echo "- S3 Bucket: $BUCKETNAME"
